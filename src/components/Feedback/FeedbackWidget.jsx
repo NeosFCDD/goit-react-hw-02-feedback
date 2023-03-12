54cf1db75565d765da69a7d9a4127f3482f93096
Volodymyr Hudzula <110738058+NeosFCDD@users.noreply.github.com>
2023-03-12 21:41:26 +0200

    Update FeedbackWidget.jsx

--- src/components/Feedback/FeedbackWidget.jsx
+++ src/components/Feedback/FeedbackWidget.jsx
@@ -7,16 +7,25 @@ export default class FeedbackWidget extends Component {
       neutral: 0,
       bad: 0,
     };
-  
+    
+    handleIncrement = e=> {
+        this.setState (prevState => {
+            const target = e.target.textContent;
+            return {
+                [target]: prevState[target]+1,
+            };
+        });
+    };
+
     render() {
       return (
         <div className = "wrapper">
           <div>
             <h2>Please leave feedback</h2>
             <div>
-                <button>Good</button>
-                <button>Neutral</button>
-                <button>Bad</button>
+                <button type="button" onClick={this.handleIncrement}>Good</button>
+                <button type="button" onClick={this.handleIncrement}>Neutral</button>
+                <button type="button" onClick={this.handleIncrement}>Bad</button>
             </div>
           </div>
 
