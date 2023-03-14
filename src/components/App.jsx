a3ee6ada1d9f8dcb1dc25d255616bcaabbd252ca
Volodymyr Hudzula <110738058+NeosFCDD@users.noreply.github.com>
2023-03-14 19:42:40 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import FeedbackWidget from './Feedback/FeedbackWidget';
+import React, {Component} from "react";
 
-export const App = () => {
-  return (
-    <div>
-      <FeedbackWidget />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  
+  handleIncrement = (e) => {
+      const target = e.target.name;
+      this.setState ((prevState) => {
+          return {
+              [target]: prevState[target]+1};
+      });
+  };
+
+  render() {
+    return (
+      <div className = "wrapper">
+        <div>
+          <h2>Please leave feedback</h2>
+          <div>
+              <button type="button"  onClick={this.handleIncrement}>Good</button>
+              <button type="button"  onClick={this.handleIncrement}>Neutral</button>
+              <button type="button" onClick={this.handleIncrement}>Bad</button>
+          </div>
+        </div>
+
+        <div>
+          <h2>Statistics</h2>
+          <ul>
+              <li>Good: {this.state.good}</li>
+              <li>Neutral: {this.state.neutral}</li>
+              <li>Bad: {this.state.bad}</li>
+          </ul>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
